f9b984adac760927a46a3b288f1254e1a35cb326
David <jeff.crockett99@gmail.com>
2019-01-11 21:51:55 -0700

    Edited testing class

--- src/Testing.java
+++ src/Testing.java
@@ -4,8 +4,10 @@ import java.util.*;
 public class Testing {
 	public static void main (String[] args) {
 		
+		// Creates new LinkedList object
 		LinkedList myList = new LinkedList();
 		
+		// Add list items
 		myList.add(15);
 		myList.add(13);
 		myList.add(99);
@@ -13,21 +15,23 @@ public class Testing {
 		myList.add(13);
 		myList.add(13);
 		
+		// Prints 6
 		int answer = myList.size();
 		System.out.println("Size of list: " + Integer.toString(answer));
 		
+		// Prints list
 		myList.printList();
 		
+		// Removes duplicates
 		myList.removeDuplicates();
 		
+		// Prints new list
 		System.out.println("New List");
 		myList.printList();
 		
+		// Prints 3
 		int answer2 = myList.size();
 		System.out.println("Size of list: " + Integer.toString(answer2));
-
-		
-		
 	}
 }
 
